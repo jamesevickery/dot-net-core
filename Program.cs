d77c3a7a5551534f481cf8f0c553e23ddd52fcae
james <jamesevickery.dev@gmail.com>
2016-12-08 17:42:19 +0000

    Some Generics stuff

--- Program.cs
+++ Program.cs
@@ -4,9 +4,39 @@ namespace ConsoleApplication
 {
     public class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Client client2 = new Client("Arthur", "Ford");
+            Display<Client>(client2);
+            Console.WriteLine();
+        }
+        private static void Display<T>(T person) where T : Person
+        {
+            person.Display();
+        }
+    }
+
+    abstract class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Person(string lName, string fName)
+        {
+            FirstName = fName;
+            LastName = lName;
+        }
+        public abstract void Display();
+    }
+
+    class Client : Person
+    {
+        public Client(string lName, string fName)
+        : base(fName, lName)
+        {
+        }
+        public override void Display()
+        {
+            Console.WriteLine(FirstName + " " + LastName);
         }
     }
 }
